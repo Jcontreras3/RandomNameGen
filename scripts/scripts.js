46b734640cd11198a465b29797d175a02547ab2a
Jovann Contreras <jcontreras@codestack.co>
2022-01-24 10:11:44 -0800

    Fix small details

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+let NameTxt = document.getElementById('NameTxt');
+let Btn1 = document.getElementById('Btn1');
+let DeleteTxt = document.getElementById('DeleteTxt');
+let Btn2 = document.getElementById('Btn2');
+let GroupNumTxt = document.getElementById('GroupNumTxt');
+let Btn3 = document.getElementById('Btn3');
+let Btn4 = document.getElementById('Btn4');
